353b50b668f2ee0be8fe1db2f0bd7b5b6303df8a
Égi-Matus Bence <120624900+BenceEgi@users.noreply.github.com>
2025-10-03 12:22:32 +0200

    js alapok

---
+++ table_col/script.js
@@ -0,0 +1,21 @@
+/**
+ * @type {string}
+ */
+const a = "asd";
+
+/**
+ * @type {string[]}
+ */
+const ab = ["2", "ad", "asd"];
+
+console.log(`${a}`);
+
+/**
+ * @type {{
+ * n: string, a: number
+ * }}
+ */
+const xy = {
+n: "a",
+a: 3
+}
