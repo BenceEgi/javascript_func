c1afe75ee4304465f13e34ffcf9399ed66c77cfc
Égi-Matus Bence <120624900+BenceEgi@users.noreply.github.com>
2025-11-07 12:24:45 +0100

    esemeny kezeles

--- table_row/index.js
+++ table_row/index.js
@@ -1,3 +1,6 @@
+/**
+     * @type {{nationality: string, name: string, title: string, name2: string?, title2: string?}}
+     */
 const arr = [
     {
         nationality: "Orosz",
@@ -55,7 +58,9 @@ for (const j in arr){
         else{tr1.appendChild(td)}
     }
     tbody.appendChild(tr1);
-    tbody.appendChild(tr2);
+    if (tr2.querySelector("td")){
+        tbody.appendChild(tr2);
+    }
 }
 
 addEventListener("click", (event) => {
@@ -64,6 +69,47 @@ addEventListener("click", (event) => {
      */
     const element = event.target;
     element.classList.add("marked")
+})
+
+/**
+ * @type {HTMLFormElement}
+ */
+const form = document.getElementById("htmlform");
+addEventListener("submit", (event) => {
+    event.preventDefault();
+    /**
+     * @type {HTMLFormElement}
+     */
+    const element = event.target;
+    /**
+     * @type {HTMLInputElement[]}
+     */
+    const inputs = element.querySelectorAll("input");
+    /**
+     * @type {{nationality: string, name: string, title: string, name2: string, title2: string?}}
+     */
+    const dataStructTemplate = {
+        nationality: inputs[0].value,
+        name: inputs[1].value,
+        title: inputs[2].value,
+        name2: inputs[3].value,
+        title2: inputs[4].value
+    };
+    const tr = document.createElement("tr");
+    let td;
+    if (dataStructTemplate.name2){
+        const tr2 = document.createElement("tr");
+    }
+    for (const i in dataStructTemplate){
+        td = document.createElement("td");
+        td.innerText = dataStructTemplate[i];
+        if (dataStructTemplate.name2 && (i === "name2" || i === "title2")){
+            tr2.appendChild(td);
+        }
+        else{
+            tr.appendChild(td);
+        }
+    }
 })
 
 table.appendChild(thead);
