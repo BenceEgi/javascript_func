743146fa381b532c7caa75feb36a387cd349b9af
Égi-Matus Bence <120624900+BenceEgi@users.noreply.github.com>
2025-11-14 12:09:13 +0100

    valami

--- table_row/index.js
+++ table_row/index.js
@@ -1,8 +1,16 @@
 /**
-* @type {{nationality: string, name: string, title: string, name2: string?, title2: string?}[]}
+ * @typedef {{nationality: string, name: string, title: string, name2: string?, title2: string?}} CountryWriters
+ * @typedef {{id: string, text: string, type: string}} InputData
+ */
+
+// --- Generate Table ---
+// Data for table
+/**
+* @type {CountryWriters[]}
 */
+const fejlec = ["Nemzetiseg", "Szerző", "Mű"]
 const arr = [
-    {
+    { 
         nationality: "Orosz",
         name: "Gogol",
         title: "A köpönyeg",
@@ -27,84 +35,47 @@ const arr = [
         title: "A fizikusok",
     },
 ]
-const fejlec = ["Nemzetiseg", "Szerző", "Mű"]
 
+// Create HTMLTableElements
 const table = document.createElement("table");
-const thead = document.createElement("thead");
 const tbody = document.createElement("tbody");
 tbody.setAttribute("id", "tb");
-let tr = document.createElement("tr");
-
-// Create header
-for (const i of fejlec){
-    let th = document.createElement("th");
-    th.innerText = i;
-    tr.appendChild(th);
-}
-thead.appendChild(tr);
-
-//Add to table
-table.appendChild(thead);
 table.appendChild(tbody);
 document.body.appendChild(table);
 
+// Render table
+GenerateHeader(table, fejlec);
+renderTableBody(arr);
+
+// --- Create Forms ---
 /**
- * renders a table from the given array
- * @param data {{nationality: string, name: string, title: string, name2: string?, title2: string?}[]}
+ * @type {InputData[]}
  */
-function renderTableBody(data){
-    const tbody = document.getElementById("tb");// Get tbody with "tb" id
-    tbody.innerHTML = "";
-    let td;
-    let tr; let tr2;
-    // Generate table
-    for (const i in data){
-        tr = document.createElement("tr");
-        tr2 = document.createElement("tr");
-        for (const j in data[i]){
-            if (data[i].name2 && data[i].title2 && (j === "name2" || j === "title2")){
-                console.log(j);
-                td = document.createElement("td");
-                td.innerText = data[i][j];
-                tr2.appendChild(td);
-            }
-            else if (j !== "name2" && j !== "title2"){
-                td = document.createElement("td");
-                td.innerText = data[i][j];
-                if (data[i].name2 && data[i].title2 && (j === "nationality")){td.rowSpan = 2}
-                tr.appendChild(td);
-            }
-            td.addEventListener("click", (event) => {
-                /**
-                 * @type {HTMLTableCellElement}
-                 */
-                const element = event.target;
-                /**
-                 *
-                 * @type {HTMLTableRowElement}
-                 */
-                const parentParentElement = element.parentElement.parentElement;
+const inputs = [
+    {id:"nemzetiseg", text:"Nemzetiség:", type:"text"},
+    {id:"szerzo1", text:"Szerző:", type:"text"},
+    {id:"mu1", text:"Mű:", type:"text"},
+    {id:"szerzo2", text:"Másik Szerző:", type:"text"},
+    {id:"mu2", text:"Mű:", type:"text"}
+]
 
-                const selectedElement = parentParentElement.querySelector(".marked");
-                if (selectedElement != null){
-                    selectedElement.classList.remove("marked");
-                }
-                element.classList.add("marked");
-            })
-        }
-        tbody.appendChild(tr);
-        if (tr2.querySelector("td")){
-            tbody.appendChild(tr2);
-        }
-    }
-}
+const forms = document.createElement("form");
+const h2 = document.createElement("h2");
+h2.innerText = "Js form";
+forms.setAttribute("id", "jsForms")
+forms.appendChild(h2);
+document.body.appendChild(forms);
 
+// Render form
+renderForm(forms, inputs);
 
+// --- Events ---
+// Add row
 /**
  * @type {HTMLFormElement}
  */
-const form = document.getElementById("htmlform");
-addEventListener("submit", (event) => {
+const formJS = document.getElementById("jsForms");
+formJS.addEventListener("submit", (event) => {
     event.preventDefault();
     /**
      * @type {HTMLFormElement}
@@ -115,7 +86,7 @@ addEventListener("submit", (event) => {
      */
     const inputs = element.querySelectorAll("input");
     /**
-     * @type {{nationality: string, name: string, title: string, name2: string, title2: string?}}
+     * @type {CountryWriters}
      */
     const dataStruct = {
         nationality: inputs[0].value,
@@ -128,5 +99,125 @@ addEventListener("submit", (event) => {
     renderTableBody(arr)
 })
 
-// Render table
-renderTableBody(arr);
+// --- Form Functions ---
+/**
+ * Render out input field to form
+ * @param {HTMLFormElement} form 
+ * @param {InputData} inputData
+ */
+function renderForm(form, inputData){
+    for (let i = 0; i < inputData.length; i++)
+    {
+        createInput(form, inputData[i])
+    }
+    const button = document.createElement("button");
+    button.innerText = "Hozzáadás"
+    form.appendChild(button);
+}
+
+/**
+ * Create input fields with labels
+ * @param {HTMLFormElement} form 
+ * @param {InputData} inputData
+ */
+function createInput(form, inputData){
+    const label = document.createElement("label");
+    label.htmlFor = inputData.id;
+    label.innerText = inputData.text;
+
+    const input = document.createElement("input");
+    input.id = inputData.id;
+    input.type = inputData.type;
+    input.name = inputData.name;
+
+    form.appendChild(label);
+    form.appendChild(document.createElement("br"));
+    form.appendChild(input);
+    form.appendChild(document.createElement("br"));
+    form.appendChild(document.createElement("br"));
+}
+
+// --- Table Functions ---
+
+/**
+ * Renders a table from the given array
+ * @param data {CountryWriters[]}
+ */
+function renderTableBody(data){
+    const tbody = document.getElementById("tb");// Get tbody with "tb" id
+    tbody.innerHTML = "";
+    let td; let tr; let tr2;
+    // Generate table
+    for (const i in data){
+        tr = document.createElement("tr");
+        tr2 = document.createElement("tr");
+        for (const j in data[i]){
+            if (data[i].name2 && data[i].title2 && (j === "name2" || j === "title2")){
+                console.log(j);
+                td = createTableCell("td", data[i][j], tr2);
+            }
+            else if (j !== "name2" && j !== "title2"){
+                td = createTableCell("td", data[i][j], tr);
+                if (data[i].name2 && data[i].title2 && (j === "nationality")){td.rowSpan = 2}
+            }
+            td.addEventListener("click", (event) => {
+                /**
+                 * @type {HTMLTableCellElement}
+                 */
+                const element = event.target;
+                /**
+                 * @type {HTMLTableRowElement}
+                 */
+                const parentParentElement = element.parentElement.parentElement;
+                const selectedElement = parentParentElement.querySelector(".marked");
+                
+                if (selectedElement != null){
+                    selectedElement.classList.remove("marked");
+                }
+                element.classList.add("marked");
+            })
+        }
+        // Add to table
+        tbody.appendChild(tr);
+        if (tr2.querySelector("td")){
+            tbody.appendChild(tr2);
+        }
+    }
+}
+
+/**
+ * Create table cells
+ * @param {string} type
+ * @param {string} cellData 
+ * @param {HTMLTableRowElement} parentRow 
+ * @returns {HTMLTableCellElement}
+ */
+function createTableCell(type, cellData, parentRow){
+    /**
+     * @type {HTMLTableCellElement}
+     */
+    const cell = document.createElement(type);
+    cell.innerText = cellData;
+    parentRow.appendChild(cell);
+
+    return cell;
+}
+
+/**
+ * Generate header cells
+ * @param {HTMLTableElement} table 
+ * @param {string[]} headerList 
+ */
+function GenerateHeader(table, headerList){
+    const thead = document.createElement("thead");
+    let trHead = document.createElement("tr");
+
+    // Create header
+    for (const i of headerList){
+        createTableCell("th", i, trHead)
+    }
+    thead.appendChild(trHead);
+
+    //Add to table
+    table.appendChild(thead);
+}
